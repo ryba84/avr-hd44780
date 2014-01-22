8c33a92ebcfff5885ca79afccca54c2fddd00747
ryba84 <ryba.lodz@gmail.com>
2014-01-23 00:09:32 +0100

    Cleanup sources and repair some bugs.

--- hd44780.c
+++ hd44780.c
@@ -1,5 +1,5 @@
 #include <hd44780.h>
-
+// Read BusyFlag
 unsigned char lcdIsBusy(void)
 {
 	PORT(LCD_CONTROL_PORT) &= ~LCD_RS;
@@ -9,81 +9,119 @@ unsigned char lcdIsBusy(void)
 	}
 	return 0;
 }
+// Read data from RAM
 unsigned char lcdReadData(void)
 {
 	PORT(LCD_CONTROL_PORT) |= LCD_RS;
 	return lcdRead();
 }
+// Read data
 unsigned char lcdRead(void)
 {
 	unsigned char tmp;
 	DDR(LCD_DATA_PORT) &= ~LCD_DATA_MASK;
 	PORT(LCD_CONTROL_PORT) |= LCD_RW;
-	tmp = lcdReadNibble();
-	tmp |= (lcdReadNibble()>>4);
+	if (LCD_DATA_MASK & 0xf0)
+	{
+		tmp = lcdReadNibble();
+		tmp |= (lcdReadNibble()>>4);
+	}
+	else
+	{
+		tmp = (lcdReadNibble()<<4);
+		tmp |= lcdReadNibble();
+	}
 	return tmp;
 }
+// Read nibble from data port
 unsigned char lcdReadNibble(void)
 {
-	unsigned char tmp;
+	unsigned char inNibble;
 	PORT(LCD_CONTROL_PORT) |= LCD_E;
-	tmp = (PIN(LCD_DATA_PORT) & LCD_DATA_MASK);
+	inNibble = (PIN(LCD_DATA_PORT) & LCD_DATA_MASK);
 	PORT(LCD_CONTROL_PORT) &= ~LCD_E;
-	return tmp;
+	return inNibble;
 }
-
-void lcdSend(unsigned char dataToSend)
-{
-	DDR(LCD_DATA_PORT) |= LCD_DATA_MASK;
-	PORT(LCD_CONTROL_PORT) &= ~LCD_RW;
-	lcdSendNibble ((dataToSend << 4) & LCD_DATA_MASK);
-	lcdSendNibble (dataToSend & LCD_DATA_MASK);
-}
-
-void lcdSendNibble(unsigned char data)
-{
-	PORT(LCD_CONTROL_PORT) |= LCD_E;
-	PORT(LCD_DATA_PORT) &= ~LCD_DATA_MASK;
-	PORT(LCD_DATA_PORT) |= data;
-	PORT(LCD_CONTROL_PORT) &= ~LCD_E;
-}
-
-void lcdSendCommand(unsigned char data)
+// Send command to LCD
+void lcdSendCommand(unsigned char commandToSend)
 {
 	PORT(LCD_CONTROL_PORT) &= ~LCD_RS;
-	lcdSend(data);
+	lcdSend(commandToSend);
 }
-
+// Send char to LCD
 void lcdSendChar(unsigned char charToSend)
 {
 	PORT(LCD_CONTROL_PORT) |= LCD_RS;
 	lcdSend(charToSend);
 }
+// Send data to data
+void lcdSend(unsigned char dataToSend)
+{
+	DDR(LCD_DATA_PORT) |= LCD_DATA_MASK;
+	PORT(LCD_CONTROL_PORT) &= ~LCD_RW;
+	if (LCD_DATA_MASK & 0xf0)
+	{
+		lcdSendNibble (dataToSend & LCD_DATA_MASK);
+		lcdSendNibble ((dataToSend << 4) & LCD_DATA_MASK);
+	}
+	else
+	{
+		lcdSendNibble ((dataToSend >> 4) & LCD_DATA_MASK);
+		lcdSendNibble (dataToSend & LCD_DATA_MASK);
+	}
+}
+// Send nibble to data port
+void lcdSendNibble(unsigned char nibbleToSend)
+{
+	PORT(LCD_CONTROL_PORT) |= LCD_E;
+	PORT(LCD_DATA_PORT) &= ~LCD_DATA_MASK;
+	PORT(LCD_DATA_PORT) |= nibbleToSend;
+	PORT(LCD_CONTROL_PORT) &= ~LCD_E;
+}
 
 void lcdInit(void)
 {
 	unsigned char cnt;
-//	DDR(LCD_CONTROL_PORT) |= LCD_E | LCD_RW | LCD_RS;
-//	DDR(LCD_DATA_PORT) |= LCD_DATA_MASK;
+	DDR(LCD_CONTROL_PORT) |= LCD_E | LCD_RW | LCD_RS;
+	DDR(LCD_DATA_PORT) |= LCD_DATA_MASK;
 	_delay_ms(15);
 
 	PORT(LCD_CONTROL_PORT) &= ~(LCD_E | LCD_RW | LCD_RS);
-	for (cnt = 0; cnt < 3; cnt++)
+	PORT(LCD_DATA_PORT) &= ~LCD_DATA_MASK;
+	if (LCD_DATA_MASK & 0xf0)
 	{
+		for (cnt = 0; cnt < 3; cnt++)
+		{
+			PORT(LCD_CONTROL_PORT) |= LCD_E;
+			PORT(LCD_DATA_PORT) |= (0x03<<4);
+			PORT(LCD_CONTROL_PORT) &= ~LCD_E;
+			_delay_ms (5);
+		}
 		PORT(LCD_CONTROL_PORT) |= LCD_E;
-		PORT(LCD_DATA_PORT) &= ~LCD_DATA_MASK;
-		PORT(LCD_DATA_PORT) |= (0x03<<4);
+		PORT(LCD_DATA_PORT) |= (0x02<<4);
 		PORT(LCD_CONTROL_PORT) &= ~LCD_E;
-		_delay_ms (5);
+	}
+	else
+	{
+		for (cnt = 0; cnt < 3; cnt++)
+				{
+					PORT(LCD_CONTROL_PORT) |= LCD_E;
+					PORT(LCD_DATA_PORT) |= (0x03);
+					PORT(LCD_CONTROL_PORT) &= ~LCD_E;
+					_delay_ms (5);
+				}
+		PORT(LCD_CONTROL_PORT) |= LCD_E;
+		PORT(LCD_DATA_PORT) |= (0x02);
+		PORT(LCD_CONTROL_PORT) &= ~LCD_E;
 	}
 	_delay_ms(1);
-	lcdSendCommand(functionSet | !interface8bit | display2lines);
+	lcdSendCommand(functionSet | display2lines);
 	while (lcdIsBusy());
-	lcdSendCommand(displayOnOff | !displayOn);
+	lcdSendCommand(displayOnOff);
 	while (lcdIsBusy());
 	lcdSendCommand(clearDisplay);
 	while (lcdIsBusy());
 	lcdSendCommand(entryModeSet | cursorIncrement);
 	while (lcdIsBusy());
-	lcdSendCommand(displayOnOff | displayOn | !cursorOn | !cursorBlink);
+	lcdSendCommand(displayOnOff | displayOn);
 }
--- hd44780.h
+++ hd44780.h
@@ -10,12 +10,12 @@
 //=================================================================
 #define LCD_CONTROL_PORT D
 #define LCD_RS (1<<PD5)
-#define LCD_E (1<<PD6)
-#define LCD_RW (1<<PD7)
+#define LCD_RW (1<<PD6)
+#define LCD_E (1<<PD7)
 #define	LCD_DATA_PORT B
 #define LCD_DATA_MASK 0xf0
 //=================================================================
-// other needed definitions for working macros
+// other needed definitions for working defined macros
 //=================================================================
 #define PORT_(port) PORT ## port
 #define DDR_(port)  DDR  ## port
@@ -30,22 +30,21 @@
 #define clearDisplay			0x01
 #define returnHome				0x02
 #define entryModeSet			0x04
-	#define cursorIncrement		(1<<1)
-	#define shiftDisplay		(1<<0)
+	#define cursorIncrement		0x02
+	#define shiftDisplay		0x01
 #define displayOnOff			0x08
-	#define displayOn			(1<<2)
-	#define cursorOn			(1<<1)
-	#define cursorBlink			(1<<0)
+	#define displayOn			0x04
+	#define cursorOn			0x02
+	#define cursorBlink			0x01
 #define displayShift			0x10
-	#define shiftCursorLeft		(0<<0)
-	#define shiftCursorRight	(1<<2)
-	#define shiftDisplayLeft	(1<<3)
-	#define shiftDisplayRight	(3<<2)
+	#define shiftCursorRight	0x04
+	#define shiftDisplayLeft	0x08
+	#define shiftDisplayRight	0x0c
 #define functionSet				0x20
-	#define interface8bit		(1<<4)
-	#define display2lines		(1<<3)
-	#define font5x11			(1<<2)
-#define lcdBusyFlag				(1<<7)
+	#define interface8bit		0x10
+	#define display2lines		0x08
+	#define font5x11			0x04
+#define lcdBusyFlag				0x80
 #define setDdramAddress			0x80
 	#define firstLine			0x00
 	#define secondLine			0x40
@@ -56,10 +55,10 @@ unsigned char lcdIsBusy(void);
 unsigned char lcdReadData();
 unsigned char lcdRead();
 unsigned char lcdReadNibble();
-void lcdSend(unsigned char dataToSend);
-void lcdSendNibble(unsigned char data);
 void lcdSendCommand(unsigned char data);
 void lcdSendChar(unsigned char charToSend);
+void lcdSend(unsigned char dataToSend);
+void lcdSendNibble(unsigned char data);
 void lcdInit(void);
 //=================================================================
 // EOF
